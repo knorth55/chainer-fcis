ab714b7fa0021e80f2d2131d07c9c4f96543f3c6
Shingo Kitagawa <shingogo@hotmail.co.jp>
2017-10-06 20:38:54 +0900

    update setup.py and add requirement.txt

--- setup.py
+++ setup.py
@@ -8,5 +8,14 @@ version = '0.0.0'
 setup(
     name='fcis',
     version=version,
-    packages=find_packages()
+    packages=find_packages(),
+    scripts=['scripts/convert_model.py'],
+    install_requires=open('requirements.txt').readlines(),
+    description='Chainer Implementation of FCIS',
+    long_description=open('README.md').read(),
+    author='Shingo Kitagawa',
+    author_email='shingogo.5511@gmail.com',
+    url='https://github.com/knorth55/chainer-fcis',
+    license='MIT',
+    keywords='machine-learning',
 )
