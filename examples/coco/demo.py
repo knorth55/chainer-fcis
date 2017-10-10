fc2d43c209a86d2b64d79a8fa49f7dc1a2f036ad
Shingo Kitagawa <shingogo@hotmail.co.jp>
2017-10-10 14:22:30 +0900

    remove debug code

--- examples/coco/demo.py
+++ examples/coco/demo.py
@@ -54,7 +54,6 @@ def main():
 
     imagedir = osp.join(filepath, 'images')
     image_names = sorted(os.listdir(imagedir))
-    # image_names = ['COCO_test2015_000000000275.jpg']
 
     for image_name in image_names:
         imagepath = osp.join(imagedir, image_name)
