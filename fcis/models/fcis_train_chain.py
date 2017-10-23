79821c58ead7b84c5adc03440a6402ad10a0dd82
Shingo Kitagawa <shingogo@hotmail.co.jp>
2017-10-23 22:35:26 +0900

    refactor fcis_train_chain.py

--- fcis/models/fcis_train_chain.py
+++ fcis/models/fcis_train_chain.py
@@ -12,22 +12,20 @@ import numpy as np
 
 class FCISTrainChain(chainer.Chain):
 
-    def __init__(
-            self, fcis, rpn_sigma=3., roi_sigma=1.,
-            anchor_target_creator=AnchorTargetCreator(),
-            proposal_target_creator=ProposalTargetCreator()
-    ):
+    def __init__(self, fcis, rpn_sigma=3.0, roi_sigma=1.0):
         super(FCISTrainChain, self).__init__()
         with self.init_scope():
             self.fcis = fcis
         self.rpn_sigma = rpn_sigma
         self.roi_sigma = roi_sigma
 
-        self.anchor_target_creator = anchor_target_creator
-        self.proposal_target_creator = proposal_target_creator
-
         self.loc_normalize_mean = fcis.loc_normalize_mean
         self.loc_normalize_std = fcis.loc_normalize_std
+
+        self.anchor_target_creator = AnchorTargetCreator()
+        self.proposal_target_creator = ProposalTargetCreator(
+            loc_normalize_mean=self.loc_normalize_mean,
+            loc_normalize_std=self.loc_normalize_std)
 
     def __call__(self, x, bboxes, masks, labels, scale):
         if isinstance(bboxes, chainer.Variable):
@@ -38,7 +36,7 @@ class FCISTrainChain(chainer.Chain):
             labels = labels.data
         if isinstance(scale, chainer.Variable):
             scale = scale.data
-        scale = np.asscalar(chainer.cuda.to_cpu(scale))
+        scale = chainer.cuda.to_cpu(scale)[0]
         n = bboxes.shape[0]
         # batch size = 1
         if n != 1:
@@ -46,19 +44,22 @@ class FCISTrainChain(chainer.Chain):
 
         _, _, H, W = x.shape
         img_size = (H, W)
-        h = self.fcis.res1(x)
-        h = self.fcis.res2(h)
+        assert img_size == masks.shape[2:]
+
+        with chainer.using_config('train', False):
+            h = self.fcis.res1(x)
+            h = self.fcis.res2(h)
         h = self.fcis.res3(h)
         h = self.fcis.res4(h)
-        features = h
-        h = self.res5(h)
-
-        h = F.relu(self.psroi_conv1(h))
-        h_seg = self.psroi_conv2(h)
-        h_locs = self.psroi_conv3(h)
 
         rpn_locs, rpn_scores, rois, roi_indices, anchor = self.fcis.rpn(
-            features, (H, W), scale)
+            h, (H, W), scale)
+
+        h = self.fcis.res5(h)
+
+        h = F.relu(self.fcis.psroi_conv1(h))
+        h_seg = self.fcis.psroi_conv2(h)
+        h_locs = self.fcis.psroi_conv3(h)
 
         # batch size = 1
         bboxes = bboxes[0]
@@ -66,16 +67,13 @@ class FCISTrainChain(chainer.Chain):
         labels = labels[0]
         rpn_scores = rpn_scores[0]
         rpn_locs = rpn_locs[0]
-        rois = rois
 
         # Sample RoIs and forward
-        sample_rois, gt_roi_masks, gt_roi_locs, gt_roi_labels = \
-            self.proposal_target_creator(
-                rois, masks, bboxes, labels,
-                self.loc_normalize_mean, self.loc_normalize_std)
+        sample_rois, gt_roi_locs, gt_roi_masks, gt_roi_labels = \
+            self.proposal_target_creator(rois, bboxes, masks, labels)
 
         sample_roi_indices = self.xp.zeros(
-            (len(sample_rois),), dtype=np.int32)
+            (len(sample_rois),), dtype=np.float32)
         sample_indices_and_rois = self.xp.concatenate(
             (sample_roi_indices[:, None], sample_rois), axis=1)
 
@@ -83,6 +81,7 @@ class FCISTrainChain(chainer.Chain):
             self.fcis._pool_and_predict(
                 sample_indices_and_rois, h_seg, h_locs,
                 gt_roi_labels=gt_roi_labels)
+        roi_locs = roi_locs.reshape((len(roi_locs), -1))
 
         # RPN losses
         gt_rpn_locs, gt_rpn_labels = self.anchor_target_creator(
