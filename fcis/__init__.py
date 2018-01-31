0859c8f8e39c6bac2775c5857cb661a718abf44a
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-01-31 11:02:53 +0900

    add __version__ info in module

--- fcis/__init__.py
+++ fcis/__init__.py
@@ -1,3 +1,5 @@
+import pkg_resources
+
 from fcis import dataset  # NOQA
 from fcis import datasets  # NOQA
 from fcis import extensions  # NOQA
@@ -5,3 +7,6 @@ from fcis import functions  # NOQA
 from fcis import mask  # NOQA
 from fcis import models  # NOQA
 from fcis import utils  # NOQA
+
+
+__version__ = pkg_resources.get_distribution('fcis').version
